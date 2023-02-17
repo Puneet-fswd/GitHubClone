07ff2c55325708c88b52a269be468aedb809449f
Puneet Amrutkar <115424584+Puneet-fswd@users.noreply.github.com>
2023-02-18 00:27:31 +0530

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+function hrefFunction() {
+    location.href='repopage.html';
+    var name = document.getElementById('login').value;
+    document.getElementById('showName').innerHTML = name;
+}
+    
